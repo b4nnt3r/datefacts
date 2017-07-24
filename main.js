62a4ed35d6b5f50f47b371c9675f3cb093e94c13
Brandon Walden <brandonshanewalden@gmail.com>
2017-07-24 16:54:23 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let moment = require('moment');
+const chalk = require('chalk');
+let specifics = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+let day = moment().format('DDDo');
+let start = moment().startOf('minute');
+let now = moment();
+let midnight = moment(now);
+midnight.hour(0);
+midnight.minute(0);
+midnight.second(0);
+let seconds = start.diff(midnight, 'seconds')
+
+let savings = moment().isDST();
+if (savings === true) {
+  savings = "is";
+} else {
+  savings = "isn't";
+}
+
+let leap = moment().isLeapYear()
+if (leap === true) {
+  leap = "is";
+} else {
+  leap = "is not";
+}
+
+
+console.log(moment);
+console.log('It is ' + chalk.cyanBright(specifics));
+console.log('It is the ' + chalk.magentaBright(day) + ' of the year.');
+console.log('It is ' + chalk.redBright(seconds) + ' seconds into the day.');
+console.log('It ' + chalk.yellowBright(savings) + ' during Daylight Savings Time.');
+console.log('It ' + chalk.cyanBright(leap) + ' a leap year');
